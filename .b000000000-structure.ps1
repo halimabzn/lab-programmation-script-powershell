a5a59cbf14634383d0719f341a286b3504016056
halima fadde <43480203+halimabzn@users.noreply.github.com>
2020-04-02 21:33:49 -0400

    Delete .b000000000-structure.ps1

--- .b000000000-structure.ps1
+++
@@ -1,6 +0,0 @@
-      PID    PPID    PGID     WINPID   TTY         UID    STIME COMMAND
-I    1776   24364    1776       5100  pty0      197609 10:07:03 /usr/bin/bash
-    15036   10552   15036      19384  pty1      197609 15:36:34 /usr/bin/bash
-    10552       1   10552      10552  ?         197609 15:36:34 /usr/bin/mintty
-    18116   15036   18116      18632  pty1      197609 15:42:35 /usr/bin/PS
-    24364       1   24364      24364  ?         197609 10:07:02 /usr/bin/mintty
