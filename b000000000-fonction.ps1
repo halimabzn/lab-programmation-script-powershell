e11ee9a3c5b02afbe7d4d7005754e14483711939
halimabzn <halimafd79@gmail.com>
2020-04-02 21:24:55 -0400

    Tutoriel complété

--- b000000000-fonction.ps1
+++ b000000000-fonction.ps1
@@ -1,21 +1,30 @@
 <#
 .SYNOPSIS
     Ce script est un laboratoire Powershell
-
 .DESCRIPTION
     Ce script est utilisé pour le laboratoire de programmation en Powershell.
-
 .NOTES
-    Author: CollegeBoreal
+    Author: halimabzn
     Derniere mise à jour: yyyy-mm-dd
-
 #>
 
 # Definition de la fonction
-function Stagiaire {
+function Stagiaire
+ {
+ [CmdletBinding()]
+ param (
+        [String]$personneNom,
+        [Int]$personneAge
+    )
 
+ # message de bienvenue 
+    BEGIN {Write-Verbose "Début du script"}
+    PROCESS { "Bonjour {0} ! Tu as {1} ans." -F $personneNom, $personneAge }
+    END {Write-Verbose "Fin du script"}
 }
 
 # Appel de la fonction
-Stagiaire
+Stagiaire Toronto 35
+Stagiaire "Pascal Siakam" 26  -verbose
 
+Get-Help Stagiaire
